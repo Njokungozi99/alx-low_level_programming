0d5521e3442f53d25d0409dc8fb978944192d4a4
Njoku Ngozi <njokungozi99@gmail.com>
2022-10-26 06:09:37 -0700

    coding

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,23 +1,20 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * list_len - returns number of nodes starting at element h
+ * listint_len - returns the number of elements in a linked lists
+ * @h: linked list of type listint_t to traverse
  *
- * @h: first node of linked list to count from
- *
- * Return: number of nodes printed
+ * Return: number of nodes
  */
-size_t list_len(const list_t *h)
+size_t listint_len(const listint_t *h)
 {
-	size_t len = 0;
+	size_t n = 0;
 
-	if (h == NULL)
-		return (0);
-	while (h != NULL)
+	while (h)
 	{
-		len++;
+		n++;
 		h = h->next;
 	}
-	return (len);
+
+	return (n);
 }
